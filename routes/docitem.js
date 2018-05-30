70f67f00010124b51297437f47c6a12d00198f04
Helenqian <376112517@qq.com>
2018-05-30 23:34:46 +0800

    blndata

--- routes/docitem.js
+++ routes/docitem.js
@@ -33,7 +33,7 @@ router.get('/api/docitem', function (req, res, next) {
       if (err) return next(err);
       //var data =[];
      // var _page = req.query.page;
-      var _limit = req.query.limit;
+      //var _limit = req.query.limit;
       //for (var j = (_page - 1) * _limit ; j < _page * _limit && (accdocs[j] != null); j++)
         for(var j = 0; j<accdocs.length; j++){
             if(req.query.date){
@@ -42,6 +42,7 @@ router.get('/api/docitem', function (req, res, next) {
             dt.setFullYear(e.substring(0,4),e.substring(5,7),e.substring(8,10));
             console.log("dt"+dt);
             if((dt>mindate || dt==mindate) && (dt<maxdate || dt==maxdate)){
+                console.log("!");
                 numset.push(accdocs[j].num);
             }
             }
@@ -67,7 +68,7 @@ router.get('/api/docitem', function (req, res, next) {
         console.log(numset);    
         DocumentItem.find({ num: {  $in: numset } }).populate('header').populate('account')
         .exec(function(err, docitem){
-           console.log(docitem);
+           //console.log(docitem);
            var data=[];
            var _page = req.query.page;
            var _limit = req.query.limit;
@@ -91,50 +92,6 @@ router.get('/api/docitem', function (req, res, next) {
         });
     });
 });
-
-
-/*
-router.get('/api/docitem', function (req, res, next) {
-    let option ={}; 
-    var min = ""; var max = "";
-    var mindate = new Date(); var maxdate = new Date();
-    if(req.query.date) {
-        min = req.query.date.substring(0,10);
-        console.log("min"+min);
-        max = req.query.date.substring(13,23);
-        console.log("max"+max);
-        mindate.setFullYear(min.substring(0,4),min.substring(5,7),min.substring(8,10));
-        maxdate.setFullYear(max.substring(0,4),max.substring(5,7),max.substring(8,10));
-        console.log("mindate"+mindate);
-        console.log("maxdate"+maxdate);
-    }
-    AccountDocument.find()
-    DocumentItem.find(option).populate('account').populate('header').exec(function (err, docitem) {
-        if (err) return next(err);
-        //console.log("date:"+req.query.date);
-        var data =[];
-        var _page = req.query.page;
-        var _limit = req.query.limit;
-          for (var j = (_page - 1) * _limit ; j < _page * _limit && (docitem[j] != null); j++){
-              var o = {};
-              o.id = docitem[j].id;
-              o.num = docitem[j].num;
-              o.debit = docitem[j].debit;
-              o.credit = docitem[j].credit;
-              o.account = docitem[j].account.name;
-              o.header = docitem[j].header.name;
-              data.push(o);
-          }
-                var responsedata = {
-                code: 0,
-                msg: "",
-                count: data.length,
-                data: data
-                  } 
-                res.send(responsedata);
-      }); 
-});
-*/
 
 router.get('/glacc', function (req, res, next) {
         res.render('accbook/glacc');
--- routes/sheet.js
+++ routes/sheet.js
@@ -43,6 +43,178 @@ router.get('/api/blsheet', function(req, res, next){
         });
 });
 
+router.get('/generatebl', function(req, res, next){
+    var curryear = "2018", currmonth = "201805";
+    BlnSheet.find({period: "template"}, function (err, blnitems) {
+    Account.aggregate({$project: { _id: 1, month: 1, year: 1 } })
+    .unwind('month').unwind('year').exec(function (err, acc){
+    //Account.find({})
+    //.unwind('month').unwind('year').exec(function (err, acc){
+    //console.log(acc);
+    var visit = [];
+    for(var i=0; i<acc.length; i++){
+        visit[i]=false;
+    }
+    var blndata = [];
+    for(var j=0; j<blnitems.length; j++){
+        var o = {};
+        var cash = {}, short = {}, other = {}, stock = {}, intangible ={};
+        cash.startbln = "0.00"; cash.endbln = "0.00";
+        stock.startbln = "0.00"; stock.endbln = "0.00";
+        other.startbln = "0.00"; other.endbln = "0.00";
+        intangible.startbln = "0.00"; intangible.endbln = "0.00";
+        short.startbln = "0.00"; short.endbln = "0.00";
+
+        o.assproj  = blnitems[j].assproj;
+        o.assnum  = blnitems[j].assnum;
+        o.assendbln  = blnitems[j].assendbln;
+        o.assstartbln  = blnitems[j].assstartbln;
+        o.liabproj  = blnitems[j].liabproj;
+        o.liabnum  = blnitems[j].liabnum;
+        o.liabendbln  = blnitems[j].liabendbln;
+        o.liabstartbln  = blnitems[j].liabstartbln;
+
+        for(var i=0; i<acc.length; i++){
+            //货币资金
+            if(acc[i].num == '1001' || acc[i].num == '1002'
+                || acc[i].num == '1003' ){
+                    for(var s=0; s<acc[i].year.length; s++){
+                        if(acc[i].year[s].num == curryear){
+                            cash.startbln = (parseFloat(cash.startbln)+
+                            parseFloat(acc[i].year[s].startbln))+"";
+                    }
+                   }
+                   for(var s=0; s<acc[i].month.length; s++){
+                    if(acc[i].month[s].num == currmonth){
+                        cash.endbln = (parseFloat(cash.endbln)+
+                        parseFloat(acc[i].month[s].endbln))+"";
+                    }
+                   }
+                continue;
+            }
+            //短期投资
+            if(acc[i].num == '1101' || acc[i].num == '1102' ){
+                    for(var s=0; s<acc[i].year.length; s++){
+                        if(acc[i].year[s].num == curryear){
+                            short.startbln = (parseFloat(short.startbln)+
+                            parseFloat(acc[i].year[s].startbln))+"";
+                    }
+                   }
+                   for(var s=0; s<acc[i].month.length; s++){
+                    if(acc[i].month[s].num == currmonth){
+                        short.endbln = (parseFloat(short.endbln)+
+                        parseFloat(acc[i].month[s].endbln))+"";
+                    }
+                   }
+                continue;
+            }
+            //存货
+            /*
+            if(acc[i].num.indexOf("12") == 0 ){
+                for(var s=0; s<acc[i].year.length; s++){
+                    if(acc[i].year[s].num == curryear){
+                        stock.startbln = (parseFloat(stock.startbln)+
+                        parseFloat(acc[i].year[s].startbln))+"";
+                }
+               }
+               for(var s=0; s<acc[i].month.length; s++){
+                if(acc[i].month[s].num == currmonth){
+                    stock.endbln = (parseFloat(stock.endbln)+
+                    parseFloat(acc[i].month[s].endbln))+"";
+                }
+               }
+            continue;
+            }
+            */
+            //无形资产intangible
+            if(acc[i].num == '1801' || acc[i].num == '1805' ){
+                for(var s=0; s<acc[i].year.length; s++){
+                    if(acc[i].year[s].num == curryear){
+                        intangible.startbln = (parseFloat(intangible.startbln)+
+                        parseFloat(acc[i].year[s].startbln))+"";
+                }
+               }
+               for(var s=0; s<acc[i].month.length; s++){
+                if(acc[i].month[s].num == currmonth){
+                    intangible.endbln = (parseFloat(intangible.endbln)+
+                    parseFloat(acc[i].month[s].endbln))+"";
+                }
+               }
+            continue;
+            }
+            //其他长期资产
+            if(acc[i].num == '1815' || acc[i].num == '1911' ){
+                for(var s=0; s<acc[i].year.length; s++){
+                    if(acc[i].year[s].num == curryear){
+                        other.startbln = (parseFloat(other.startbln)+
+                        parseFloat(acc[i].year[s].startbln))+"";
+                }
+               }
+               for(var s=0; s<acc[i].month.length; s++){
+                if(acc[i].month[s].num == currmonth){
+                    other.endbln = (parseFloat(other.endbln)+
+                    parseFloat(acc[i].month[s].endbln))+"";
+                }
+               }
+            continue;
+            }
+            if(acc[i].name == o.assproj) {
+               //找到了对应的资产
+               for(var s=0; s<acc[i].year.length; s++){
+                    if(acc[i].year[s].num == curryear){
+                        o.assstartbln = acc[i].year[s].startbln;
+                    }
+               }
+               for(var s=0; s<acc[i].month.length; s++){
+                if(acc[i].month[s].num == currmonth){
+                    o.assendbln = acc[i].month[s].endbln;
+                }
+               }
+           }
+           else if(acc[i].name == o.liabproj){
+               //找到了对应的负债
+               for(var s=0; s<acc[i].year.length; s++){
+                if(acc[i].year[s].num == curryear){
+                    o.liabstartbln = acc[i].year[s].startbln;
+                }
+              }
+              for(var s=0; s<acc[i].month.length; s++){
+                if(acc[i].month[s].num == currmonth){
+                o.liabendbln = acc[i].month[s].endbln;
+              }
+             }
+           }
+        }
+        if(o.assproj == "货币资金") {o.assstartbln = cash.startbln; o.assendbln = cash.endbln;}
+        else if (o.assproj == "短期投资") {o.assstartbln = short.startbln; o.assendbln = short.endbln;}
+        else if (o.assproj == "存货") {o.assstartbln = stock.startbln; o.assendbln = stock.endbln;}
+        else if (o.assproj == "无形资产") {o.assstartbln = intangible.startbln; o.assendbln = intangible.endbln;}
+        else if (o.assproj == "其他长期资产") {o.assstartbln = other.startbln; o.assendbln = other.endbln;}
+        blndata.push(o);
+    }
+    console.log(blndata);
+    /*
+        var di = new DocumentItem({
+            id : docitem.id,
+            num : docitem.num,
+            debit : docitem.debit,
+            credit : docitem.credit,
+            header : docitem.header,
+            account : docitem.account,
+            yearnum : yearnum,
+            monthnum : monthnum
+            });
+            di.save(function(err){
+            if(err) return next(err);
+            console.log('New docitem has been created   ');
+            //return res.redirect('/addaccdoc');
+            });
+            }
+            */
+    });
+    });
+});
+
 router.get('/addblnsheet', function(req, res, next) {
     res.render('sheet/addblnsheet');
 });
